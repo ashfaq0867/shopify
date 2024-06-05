1708a889cfe52b3ad1a493641cdf0a1b2f36599e
Jawad Zahid <jawad.zahid@tecspine.com>
2024-06-05 13:08:16 +0500

    datatables changes

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -12,6 +12,9 @@
     <link rel="stylesheet" href="{{ asset('plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
     <link rel="stylesheet" href="{{ asset('plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+
+
 
 
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
